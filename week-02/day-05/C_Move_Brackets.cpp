f2bde8360837db1475f6554224886dfeae552388
mahabubr <mahabub/r753@gmail.com>
2024-03-26 16:00:40 +0600

    Day 5 DONE

---
+++ week-02/day-05/C_Move_Brackets.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define B_INF LLONG_MAX
+#define  S_INF LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    stack<char> st;
+
+    int cnt = 0;
+
+    for (char c : s) {
+        if (c == '(') {
+            st.push(c);
+        }
+        else if (!st.empty() && c == ')') {
+            st.pop();
+        }
+        else {
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
