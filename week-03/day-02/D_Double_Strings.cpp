08f88d91267aafbeb6de01e374060ebd3356d82b
mahabubr <mahabub/r753@gmail.com>
2024-03-30 20:22:31 +0600

    Day  2 Done

--- week-03/day-02/D_Double_Strings.cpp
+++ week-03/day-02/D_Double_Strings.cpp
@@ -26,16 +26,28 @@ void solve() {
         st.insert(arr[i]);
     }
 
+    vector<int> v;
+
     for (int i = 0; i < n - 1; i++) {
         bool found = false;
 
         for (int j = i + 1; j < n; j++) {
-            if (st.count(arr[i]) && st.count(arr[j])) {
+            string conS = arr[i] + arr[j];
+            if (st.find(conS) != st.end()) {
                 found = true;
                 break;
             }
         }
-        cout << found << endl;
+        if (found) {
+            v.push_back(1);
+        }
+        else {
+            v.push_back(0);
+        }
+    }
+
+    for (int n : v) {
+        cout << n << " ";
     }
 
 
