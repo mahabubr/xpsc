736c16299519e7bdd0fc809aacba1084a742de90
mahabubr <mahabub/r753@gmail.com>
2024-04-01 22:54:38 +0600

    Day 4 DONE

---
+++ week-03/day-04/C_1_Powering_the_Hero_easy_version.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define m_inf LLONG_MAX
+#define s_inf LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █            ███    ██             ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █            ██           ██            ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+// 
+
+
+void infinite() {
+    // write code
+    ll n;
+    cin >> n;
+
+    vi arr(n);
+
+    for (ll i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    priority_queue<ll> pq;
+
+    ll cnt = 0;
+
+    for (ll i = 0; i < n; i++) {
+        if (arr[i] == 0) {
+            if (!pq.empty()) {
+                cnt += pq.top();
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(arr[i]);
+        }
+    }
+
+    cout << cnt << endl;
+
+
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        infinite();
+    }
+
+    return 0;
+}
