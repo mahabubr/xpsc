ca0ec85b4e1c7dfcf4831d7b206457a61e3e97c1
mahabubr <mahabub/r753@gmail.com>
2024-04-06 21:06:55 +0600

    Day 2 DONE

---
+++ week-04/day-02/A_We_Need_the_Zero.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define m_inf LLONG_MAX
+#define s_inf LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █            ███    ██             ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █            ██           ██            ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+// 
+
+
+void infinite() {
+    // write code
+    int n;
+    cin >> n;
+
+    vi arr(n);
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bool flag = true;
+    int ans = 0;
+
+    for (int i = 0; i < (1 << 8); i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum ^= arr[j] ^ i;
+        }
+
+        if (sum == 0) {
+            flag = false;
+            ans = i;
+            break;
+        }
+    }
+
+    if (!flag) {
+        cout << ans << endl;
+    }
+    else {
+        cout << -1 << endl;
+    }
+
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        infinite();
+    }
+
+    return 0;
+}
---
+++ week-04/day-02/Playing_with_OR.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define m_inf LLONG_MAX
+#define s_inf LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █            ███    ██             ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █            ██           ██            ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+// 
+
+// if all even then even
+// if one odd then odd
+
+void infinite() {
+    // write code
+    int n, k;
+    cin >> n >> k;
+
+    vi arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int l = 0, r = 0, sum = 0;
+
+    int oddCount = 0, ans = 0;
+
+    while (r < n) {
+        if (arr[r] % 2 != 0) {
+            oddCount++;
+        }
+
+
+        if (r - l + 1 == k) {
+            if (oddCount > 0) {
+                ans++;
+            }
+
+            if (arr[l] % 2 != 0) {
+                oddCount--;
+            }
+
+
+            l++;
+        }
+
+        r++;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        infinite();
+    }
+
+    return 0;
+}
