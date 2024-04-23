5b1a402101418bc7e92a5185c0606a05b1e0c557
mahabubr <mahabub/r753@gmail.com>
2024-04-24 01:34:06 +0600

    Day 6 done

---
+++ week-05/day-06/A_New_Palindrome.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define m_inf LLONG_MAX
+#define s_inf LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █            ███    ██             ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █            ██           ██            ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+// 
+
+
+void infinite() {
+    // write code
+    string s;
+    cin >> s;
+
+    set<char> st;
+
+    for (int i = 0; i < s.size() / 2; i++) {
+        st.insert(s[i]);
+    }
+
+    if (st.size() > 1) {
+        yes;
+    }
+    else {
+        no;
+    }
+
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        infinite();
+    }
+
+    return 0;
+}
---
+++ week-05/day-06/B_BerSU_Ball.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define dl double
+#define pi pair<int, int>
+#define m_inf LLONG_MAX
+#define s_inf LLONG_MIN
+#define vi vector<int>
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █            ███    ██             ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █            ██           ██            ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+// 
+
+
+void infinite() {
+    // write code
+    int n;
+    cin >> n;
+
+    vi a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int m;
+    cin >> m;
+
+    vi b(m);
+
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int ans = 0;
+
+    vector<bool> vis(m, false);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!vis[j] && abs(a[i] - b[j]) <= 1) {
+                ans++;
+                vis[j] = true;
+                break;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    infinite();
+
+    return 0;
+}
