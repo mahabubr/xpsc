fe78e7957b177a269d7142b61d4de50fc4d44dc2
mahabubr <mahabub/r753@gmail.com>
2024-04-28 22:57:32 +0600

    Day 2 Done

---
+++ week-06/Codeforces Round 941 (Div. 2)/A_Card_Exchange.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define pi pair<int, int>
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+typedef long long int ll;
+typedef double dl;
+typedef unsigned long int ull;
+
+const ll N = 2e5 + 9;
+const ll MOD = 1e9 + 7;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    int n, k;
+    cin >> n >> k;
+
+    map<int, int> mp;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        mp[x]++;
+    }
+
+    int mx = INT_MIN;
+
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        mx = max(mx, it->second);
+    }
+
+    if (mx < k) {
+        cout << n << endl;
+    }
+    else {
+        int ans = min(n, k - 1);
+        cout << ans << endl;
+    }
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    // int t = 1;
+    int t;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
