bb96774a8b10564b9951ee72dfdffc83c1a26674
mahabubr <mahabub/r753@gmail.com>
2024-04-30 23:04:12 +0600

    Day 4 done

---
+++ week-06/Practice Contest 5 - Div. 3/C_Another_Good_String.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    int n, q;
+    cin >> n >> q;
+
+    string s;
+    cin >> s;
+
+    int mx = 1, ans = 1;
+
+    rep(i, 1, n - 1) {
+        if (s[i] == s[i - 1]) {
+            mx++;
+
+            ans = max(ans, mx);
+        }
+        else {
+            mx = 1;
+        }
+    }
+
+    cout << ans << " ";
+
+
+    int queries = q, i = n;
+
+    while (queries--) {
+        string str = s;
+
+        char x;
+        cin >> x;
+        s += x;
+
+        if (s[i] == s[i - 1]) {
+            mx++;
+
+            ans = max(ans, mx);
+        }
+        else {
+            mx = 1;
+        }
+
+        cout << ans << " ";
+
+        i++;
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
---
+++ week-06/day-04/D_Wooden_Toy_Festival.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    ll n;
+    cin >> n;
+
+    vll arr(n);
+
+    rep(i, 0, n - 1) {
+        cin >> arr[i];
+    }
+
+    sort(all(arr));
+
+    auto Possible = [&](ll mid)->bool {
+        ll cnt = 1;
+
+        int l = 0, r = 1;
+
+        while (r < n) {
+            if (arr[l] + mid + mid >= arr[r]) {
+                r++;
+            }
+            else {
+                cnt++;
+                l = r;
+                r = l;
+            }
+        }
+
+        return cnt <= 3;
+        };
+
+    ll l = 0, r = 1e18, ans = 0;
+
+    while (l <= r) {
+        ll mid = (l + r) / 2;
+
+        if (Possible(mid)) {
+            ans = mid;
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
