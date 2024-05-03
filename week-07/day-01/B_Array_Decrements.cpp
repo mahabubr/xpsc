b989828d32849b551323d80a9024cbce286ca2a0
mahabubr <mahabub/r753@gmail.com>
2024-05-03 22:44:08 +0600

    Day 1 done

---
+++ week-07/day-01/B_Array_Decrements.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    int n;
+    cin >> n;
+
+    vi a(n), b(n);
+
+    rep(i, 0, n - 1) {
+        cin >> a[i];
+    }
+
+    rep(i, 0, n - 1) {
+        cin >> b[i];
+    }
+
+    int mx = 0;
+
+    rep(i, 0, n - 1) {
+        mx = max(mx, a[i] - b[i]);
+    }
+
+
+    bool flag = true;
+
+    rep(i, 0, n - 1) {
+        if (b[i] != max(a[i] - mx, 0)) {
+            flag = false;
+            break;
+        }
+    }
+
+    if (flag) {
+        yes;
+    }
+    else {
+        no;
+    }
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
