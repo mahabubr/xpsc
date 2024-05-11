9f55e41d989087e1074b5f1826198eb19f29adc0
mahabubr <mahabub/r753@gmail.com>
2024-05-11 16:15:06 +0600

    Day 1 Done

---
+++ week-08/day-01/D_Divide_and_Equalize.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<ll, ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    ll n;
+    cin >> n;
+
+    vll arr(n);
+
+    rep(i, 0, n - 1) {
+        cin >> arr[i];
+    }
+
+    mi cnt;
+
+    rep(i, 0, n - 1) {
+        for (int j = 2;j * j <= arr[i]; j++) {
+            if ((arr[i] % j) == 0) {
+                while ((arr[i] % j) == 0) {
+                    cnt[j]++;
+                    arr[i] /= j;
+                }
+            }
+        }
+
+        if (arr[i] > 1) {
+            cnt[arr[i]]++;
+        }
+    }
+
+    bool flag = true;
+
+    for (auto [x, y] : cnt) {
+        if ((y % n) != 0) {
+            flag = false;
+            break;
+        }
+    }
+
+    if (flag) {
+        yes;
+    }
+    else {
+        no;
+    }
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
