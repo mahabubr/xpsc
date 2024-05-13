b5c9c30e9dade454db5406ccaba2e3868dfa9333
mahabubr <mahabub/r753@gmail.com>
2024-05-13 12:39:48 +0600

    Day 2 Done

---
+++ week-08/day-02/A_Bachgold_Problem.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+
+#define NOT_FOUND()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<ll, ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+using namespace std;
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+
+void InFiNiTe() {
+    // write code
+    ll n;
+    cin >> n;
+
+    if (n == 3) {
+        cout << 1 << endl;
+        cout << 3 << endl;
+    }
+    else if (n % 2 == 0) {
+        cout << n / 2 << endl;
+
+        rep(i, 0, (n / 2) - 1) {
+            cout << 2 << " ";
+        }
+
+        cout << endl;
+    }
+    else {
+        n -= 3;
+
+        cout << (n / 2) + 1 << endl;
+
+        rep(i, 0, (n / 2) - 1) {
+            cout << 2 << " ";
+        }
+
+        cout << 3 << endl;
+    }
+}
+
+int main() {
+
+    NOT_FOUND();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
