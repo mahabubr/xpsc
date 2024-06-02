3db2001bba1242d639787b85847da720a17ad905
mahabubr <mahabub/r753@gmail.com>
2024-06-02 19:32:47 +0600

    Day 1 Done

---
+++ week-11/AtCoder Regular Contest 179/A_Partition.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+#define __404__()              \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define mii map<ll,ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+#define pri pair<ll,ll>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define neg cout<<"-1"<<endl
+#define ln '\n'
+#define lnn cout<<ln;
+#define lnnn cout<<ln<<ln;
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void InFiNiTe() {
+    // write code
+    ll n, k;
+    cin >> n >> k;
+
+    vll arr(n);
+
+    rep(i, 0, n - 1) {
+        cin >> arr[i];
+
+    }
+
+    sort(all(arr));
+
+
+    vll sum(n + 1, 0);
+    for (ll i = 0; i < n; ++i) {
+        sum[i + 1] = sum[i] + arr[i];
+    }
+
+    bool valid = true;
+    bool flag = false;
+    for (ll i = 0; i <= n; ++i) {
+        if (sum[i] >= k) {
+            flag = true;
+        }
+        else if (flag) {
+            valid = false;
+            break;
+        }
+    }
+
+    if (valid) {
+        yes;
+        for (const auto& x : arr) {
+            cout << x << " ";
+        }
+        cout << ln;
+    }
+    else {
+        no;
+    }
+}
+
+
+int main() {
+
+    __404__();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
