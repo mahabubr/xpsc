15b24b2ffeb4eba50d86f68837f1236c01ce457d
mahabubr <mahabub/r753@gmail.com>
2024-06-23 20:28:01 +0600

    Contest Done

---
+++ week-12/AtCoder Beginner Contest 358/C_Popcorn.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+#define __404__()              \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define mii map<ll,ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+#define pri pair<ll,ll>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define neg cout<<"-1"<<endl
+#define ln '\n'
+#define lnn cout<<ln;
+#define lnnn cout<<ln<<ln;
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+
+void InFiNiTe() {
+    // write code
+    int n, m;
+    cin >> n >> m;
+
+    vector<bitset<10>> bits(n);
+
+    rep(i, 0, n - 1) {
+        string s;
+        cin >> s;
+
+        rep(j, 0, m - 1) {
+            if (s[j] == 'o') {
+                bits[i].set(j);
+            }
+        }
+
+    }
+
+    int mn = INT_MAX;
+
+    for (int mask = 1; mask < (1 << n); mask++) {
+        int cnt = 0;
+        bitset<10> tmp;
+
+        rep(i, 0, n - 1) {
+            if (mask & (1 << i)) {
+                tmp |= bits[i];
+                cnt++;
+            }
+        }
+
+        if (tmp.count() == m) {
+            mn = min(mn, cnt);
+        }
+    }
+
+    cout << mn << ln;
+
+}
+
+int main() {
+
+    __404__();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
---
+++ week-12/Starters 139 Division 3 (Rated)/Sum_of_N.cpp
@@ -0,0 +1,215 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+#define __404__()              \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define mii map<ll,ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+#define pri pair<ll,ll>
+
+#define rep(i, s, e) for (ll i = (s); i <= (e); i++)
+#define per(i, s, e) for (ll i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)((x).size()))
+
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define neg cout<<"-1"<<endl
+#define ln '\n'
+#define lnn cout<<ln;
+#define lnnn cout<<ln<<ln;
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+const ll M = 1e6 + 2;
+
+vll fact(M, 0), p;
+
+vll primes;
+void sieve(ll limit) {
+    vector<bool> isVisit(limit + 1, true);
+
+    for (ll i = 2;i * i <= limit;i++) {
+        if (isVisit[i]) {
+            for (ll j = i + i;j <= limit;j += i) {
+                isVisit[j] = false;
+            }
+        }
+    }
+
+    for (ll i = 2;i <= limit;i++) {
+        if (isVisit[i]) {
+            primes.push_back(i);
+        }
+    }
+}
+
+void prime_fact() {
+    fact[1] = 1;
+    for (ll i = 2; i < M; i++)
+    {
+        if (fact[i] == 0)
+        {
+            fact[i] = i;
+            for (ll j = i * i; j < M; j += i)
+                if (fact[j] == 0)
+                    fact[j] = i;
+        }
+    }
+
+    rep(i, 2, M) {
+        if (i == fact[i]) {
+            p.push_back(i);
+        }
+    }
+}
+
+mii prime_factors(ll limit) {
+    mii count;
+
+    for (int i = 2;i * i <= limit;i++) {
+        if (limit % i == 0) {
+            while (limit % i == 0) {
+                count[i]++;
+                limit /= i;
+            }
+        }
+    }
+
+    if (limit > 1) {
+        count[limit]++;
+    }
+
+    return count;
+}
+
+vll divisors(ll limit) {
+    vll div;
+    for (int i = 1;i * i <= limit;i++) {
+        if (limit % i == 0) {
+            div.push_back(i);
+            if ((limit / i) != i) {
+                div.push_back(limit / i);
+            }
+        }
+    }
+
+    sort(all(div));
+
+    return div;
+}
+
+bool is_prime(ll limit) {
+    if (limit == 1) {
+        return false;
+    }
+
+    for (ll i = 2;i * i <= limit;i++) {
+        if (limit % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll gcd(ll a, ll b) {
+    if (b == 0)return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / gcd(a, b)) * b);
+}
+
+ll power(ll a, ll b)
+{
+    ll ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            ans *= a;
+        a *= a;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+void InFiNiTe() {
+    // write code
+
+    ll k;
+    cin >> k;
+
+    ll ans = 0, maintain = (fact[k] == k) ? k : fact[k];
+
+    rep(i, 0, sz(p) - 1) {
+        if (p[i] <= maintain) {
+            ans += p[i] * k;
+        }
+    }
+
+    cout << ans << ln;
+}
+
+
+int main() {
+
+    __404__();
+
+    prime_fact();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
