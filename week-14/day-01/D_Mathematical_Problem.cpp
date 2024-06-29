37e5c592336f13d0542f1aa9c4cdcfc120c2adb1
mahabubr <mahabub/r753@gmail.com>
2024-06-29 23:16:31 +0600

    day 1

---
+++ week-14/day-01/D_Mathematical_Problem.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+#define __404__()              \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long int ll;
+typedef unsigned long int ull;
+typedef double ld;
+typedef long double lld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define mi map<int, int>
+#define mii map<ll,ll>
+#define umi unordered_map<int, int>
+#define pr pair<int, int>
+#define pri pair<ll,ll>
+
+#define rep(i, s, e) for (int i = (s); i <= (e); i++)
+#define per(i, s, e) for (int i = (s); i >= (e); i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)((x).size()))
+
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define neg cout<<"-1"<<endl
+#define ln '\n'
+#define lnn cout<<ln;
+#define lnnn cout<<ln<<ln;
+
+#define b_inf LLONG_MAX
+#define s_inf LLONG_MIN
+
+const ld pi = 3.141592653589793238;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 9;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+//         ██████████████               ██████████████           //
+//    ██████              ██ ███████ ██              ███████     //
+//     ████                ███      █                 ██         //
+//       ██  █              ██     ██                █ █         //
+//       ██    █      █████████    ████████       ██   █         //
+//        █      █          ██      █            █    ██         //
+//        ██      ██       █         █        ██     ██          //
+//          █    █████   ██           ██  ███████  ██           //
+//            ███     ███               ██       ███             //
+
+// ███╗░░░███╗░█████╗░██╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░  // 
+// ████╗░████║██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗  // 
+// ██╔████╔██║███████║███████║███████║██████╦╝██║░░░██║██████╦╝  // 
+// ██║╚██╔╝██║██╔══██║██╔══██║██╔══██║██╔══██╗██║░░░██║██╔══██╗  // 
+// ██║░╚═╝░██║██║░░██║██║░░██║██║░░██║██████╦╝╚██████╔╝██████╦╝  // 
+// ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚═════╝░╚═════╝░  // 
+
+vi arr(N);
+
+void InFiNiTe() {
+    // write code
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ans = INT_MAX;
+
+    rep(i, 0, n - 2) {
+        int cnt = 0;
+        rep(j, 0, n - 1) {
+            if (i == j) {
+                arr[cnt++] = 10 * (s[j] - '0') + (s[j + 1] - '0');
+                j++;
+            }
+            else {
+                arr[cnt++] = s[j] - '0';
+            }
+        }
+        int val = 0;
+
+        rep(j, 0, cnt - 1) {
+            if (arr[j] == 0) {
+                val = -1;
+                break;
+            }
+
+            if (arr[j] != 1) {
+                val += arr[j];
+            }
+        }
+
+        if (!val) {
+            val = 1;
+        }
+
+        if (val == -1) {
+            val = 0;
+        }
+
+        ans = min(ans, val);
+    }
+
+    cout << ans << ln;
+
+}
+
+int main() {
+
+    __404__();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        InFiNiTe();
+    }
+
+    return 0;
+}
